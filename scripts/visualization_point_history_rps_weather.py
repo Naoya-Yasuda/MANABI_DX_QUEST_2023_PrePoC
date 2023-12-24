fe53e51352bd90e3de638174a81775c219f42af4
sin6160 <s.saito2048@gmail.com>
2023-12-24 23:27:10 +0900

    店ごと、日ごとの各種合計を算出（型微修正）

--- scripts/visualization_point_history_rps_weather.py
+++ scripts/visualization_point_history_rps_weather.py
@@ -10,6 +10,49 @@ def replace_nan(df):
     df = df.replace('///', np.nan)
     return df
 
+def set_dtype(df):
+    column_types = {
+        'id':np.float32,
+        'user_id':np.float64,
+        'series_id' : np.float32,
+        'shop_id' : str,
+        'shop_name' : str,
+        'card_id' : str,
+        'amount' : np.float32,
+        'amount_kg' : np.float32,
+        'point' : np.float32,
+        'total_point' : np.float32,
+        'total_amount' : np.float32,
+        'coin' : np.float32,
+        'rank_id':np.float32,
+        'use_date': 'datetime64[ns]',
+        'created_at': 'datetime64[ns]',
+        'updated_at': 'datetime64[ns]',
+        '支店ID' : np.float32,
+        'super' : str,
+        'prefectures' : str,
+        'municipality' : str,
+        'shop_name_1' :  str,
+        'shop_id_1' :    str,
+        'store_latitude' : np.double,
+        'store_longitude' : np.double,
+        '年月日' : 'datetime64[ns]',
+        '天気': str,
+        '平均気温(℃)': np.float32,
+        '最高気温(℃)': np.float32,
+        '最低気温(℃)': np.float32,
+        '降水量の合計(mm)': np.float32,
+        '平均風速(m/s)': np.float32,
+        '平均湿度(％)': np.float32,
+        '平均現地気圧(hPa)': np.float32,
+        '平均雲量(10分比)': np.float32,
+        '降雪量合計(cm)': np.float32,
+        '日照時間(時間)': np.float32,
+        '合計全天日射量(MJ/㎡)': np.float32,
+    }
+    df = df.astype(column_types)
+    return df
+
 def aggregate_shop_date(df):
     # shop_idと年月日ごとにグループ化し、合計値と代表値を計算
     aggregated_df = df.groupby(['shop_id', '年月日']).agg({
@@ -54,6 +97,8 @@ def aggregate_shop_date(df):
     # 結果を保存
     aggregated_df.to_csv('data/input/point_history_per_shop_date.csv', index=False, encoding="utf-8")
 
-
-df = pd.read_csv('data/input/point_history_rps_weather.csv', encoding="utf-8")
-aggregate_shop_date(df)
+if __name__ == '__main__':
+    df = pd.read_csv('data/input/point_history_rps_weather.csv', encoding="utf-8")
+    df = replace_nan(df)
+    df = set_dtype(df)
+    aggregate_shop_date(df)
