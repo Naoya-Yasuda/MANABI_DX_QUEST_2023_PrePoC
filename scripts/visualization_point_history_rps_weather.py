a091119fc680742332b28401dfaa12dc975f5161
sin6160 <s.saito2048@gmail.com>
2023-12-29 19:19:56 +0900

    天気データを結合するコードを修正

--- scripts/visualization_point_history_rps_weather.py
+++ scripts/visualization_point_history_rps_weather.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+# 自作モジュール
+from utils.point_history_utils import replace_nan, set_dtype, parse_date
+
 # 浮動小数点数を小数点以下3桁で表示するように設定
 pd.set_option('display.float_format', '{:.3f}'.format)
 
@@ -15,59 +18,6 @@ pd.set_option('display.float_format', '{:.3f}'.format)
 # クラウド環境用
 plt.rcParams['font.family'] = 'Noto Sans CJK JP'
 
-def replace_nan(df):
-    df = df.replace('N', np.nan)
-    df = df.replace('NaN', np.nan)
-    df = df.replace('nan', np.nan)
-    df = df.replace('foo', np.nan)
-    df = df.replace('///', np.nan)
-    return df
-
-
-
-def aggregate_shop_date(df):
-    # shop_idと年月日ごとにグループ化し、合計値と代表値を計算
-    aggregated_df = df.groupby(['shop_id', '年月日']).agg({
-        'amount': 'sum',
-        'amount_kg': 'sum',
-        'point': 'sum',
-        'total_point': 'sum',
-        'total_amount': 'sum',
-        'coin': 'sum',
-        'series_id': 'first',
-        'shop_name': 'first',
-        'リサイクル分類ID': 'first',
-        '支店ID': 'first',
-        'super': 'first',
-        'prefectures': 'first',
-        'municipality': 'first',
-        'shop_name_1': 'first',
-        'shop_id_1': 'first',
-        'store_opening_time': 'first',
-        'store_closing_time': 'first',
-        'rps_opening_time': 'first',
-        'rps_closing_time': 'first',
-        'store_latitude': 'first',
-        'store_longitude': 'first',
-        '天気': 'first',
-        '平均気温(℃)': 'first',
-        '最高気温(℃)': 'first',
-        '最低気温(℃)': 'first',
-        '降水量の合計(mm)': 'first',
-        '平均風速(m/s)': 'first',
-        '平均湿度(％)': 'first',
-        '平均現地気圧(hPa)': 'first',
-        '平均雲量(10分比)': 'first',
-        '降雪量合計(cm)': 'first',
-        '日照時間(時間)': 'first',
-        '合計全天日射量(MJ/㎡)': 'first'
-    }).reset_index()
-
-    # shop_idと年月日でソート
-    aggregated_df = aggregated_df.sort_values(by=['shop_id', '年月日'])
-
-    # 結果を保存
-    aggregated_df.to_csv('data/input/point_history_per_shop_date.csv', index=False, encoding="utf-8")
 
 def aggregate_date(df):
     # shop_idごとにグループ化し、合計値と代表値を計算
@@ -112,72 +62,6 @@ def aggregate_date(df):
 
     # 結果を保存
     aggregated_df.to_csv('data/input/point_history_per_shop.csv', index=False, encoding="utf-8")
-
-# カスタム関数を定義
-def parse_date(date):
-    try:
-        return pd.to_datetime(date)
-    except ValueError:
-        try:
-            return pd.to_datetime(date, format='%Y年%m月%d日')
-        except ValueError:
-            return pd.to_datetime(date, format='%Y/%m/%d')
-
-def replace_nan(df):
-    df = df.replace('N', np.nan)
-    df = df.replace('NaN', np.nan)
-    df = df.replace('nan', np.nan)
-    df = df.replace('foo', np.nan)
-    df = df.replace('///', np.nan)
-    return df
-
-def set_dtype(df):
-    # 列名を直感的に変更
-    df = df.rename(columns={'id_1': '支店ID'})
-    df = df.rename(columns={'item_id': 'リサイクル分類ID'})
-
-    column_types = {
-        'id':np.float32,
-        'user_id':np.float64,
-        'series_id' : np.float32,
-        'shop_id' : str,
-        'shop_name' : str,
-        'card_id' : str,
-        'リサイクル分類ID' : str,
-        'amount' : np.float32,
-        'amount_kg' : np.float32,
-        'point' : np.float32,
-        'total_point' : np.float32,
-        'total_amount' : np.float32,
-        'coin' : np.float32,
-        'rank_id':np.float32,
-        'use_date': 'datetime64[ns]',
-        'created_at': 'datetime64[ns]',
-        'updated_at': 'datetime64[ns]',
-        '支店ID' : np.float32,
-        'super' : str,
-        'prefectures' : str,
-        'municipality' : str,
-        'shop_name_1' :  str,
-        'shop_id_1' :    str,
-        'store_latitude' : np.double,
-        'store_longitude' : np.double,
-        #'年月日' : 'datetime64[ns]',
-        #'天気': str,
-        #'平均気温(℃)': np.float32,
-        #'最高気温(℃)': np.float32,
-        #'最低気温(℃)': np.float32,
-        #'降水量の合計(mm)': np.float32,
-        #'平均風速(m/s)': np.float32,
-        #'平均湿度(％)': np.float32,
-        #'平均現地気圧(hPa)': np.float32,
-        #'平均雲量(10分比)': np.float32,
-        #'降雪量合計(cm)': np.float32,
-        #'日照時間(時間)': np.float32,
-        #'合計全天日射量(MJ/㎡)': np.float32,
-    }
-    df = df.astype(column_types)
-    return df
 
 def show_total_recycle_amount_per_date(df):
     # Nanに置き換え
@@ -253,7 +137,7 @@ def aggregate_shop_date(df):
     aggregated_df = aggregated_df.sort_values(by=['shop_id', '年月日'])
 
     # 結果を保存
-    aggregated_df.to_csv('data/input/point_history_per_shop_date_noncleansing.csv', index=False, encoding="utf-8")
+    aggregated_df.to_csv('data/input/point_history_per_shop_date.csv', index=False, encoding="utf-8")
 
 def concat_csv():
     # data/input/point_history_1.csv ~ data/input/point_history_15.csv を結合。data/input/point_history.csvに保存
---
+++ utils/point_history_utils.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def replace_nan(df):
+    """
+    Replace 'N', 'NaN', 'nan', 'foo', '///' to np.nan
+    args:
+        df: pandas.DataFrame
+    return:
+        df: pandas.DataFrame    
+    """
+    df = df.replace('N', np.nan)
+    df = df.replace('NaN', np.nan)
+    df = df.replace('nan', np.nan)
+    df = df.replace('foo', np.nan)
+    df = df.replace('///', np.nan)
+    return df
+
+def parse_date(date):
+    """
+    Parse date string to datetime object
+    args:
+        date: str
+    return:
+        datetime object
+    """
+    try:
+        return pd.to_datetime(date)
+    except ValueError:
+        try:
+            return pd.to_datetime(date, format='%Y年%m月%d日')
+        except ValueError:
+            return pd.to_datetime(date, format='%Y/%m/%d')
+
+
+
+
+def set_dtype(df):
+    # 列名を直感的に変更
+    df = df.rename(columns={'id_1': '支店ID'})
+    df = df.rename(columns={'item_id': 'リサイクル分類ID'})
+
+    column_types = {
+        'id':np.float32,
+        'user_id':np.float64,
+        'series_id' : np.float32,
+        'shop_id' : str,
+        'shop_name' : str,
+        'card_id' : str,
+        'リサイクル分類ID' : str,
+        'amount' : np.float32,
+        'amount_kg' : np.float32,
+        'point' : np.float32,
+        'total_point' : np.float32,
+        'total_amount' : np.float32,
+        'coin' : np.float32,
+        'rank_id':np.float32,
+        'use_date': 'datetime64[ns]',
+        'created_at': 'datetime64[ns]',
+        'updated_at': 'datetime64[ns]',
+        '支店ID' : np.float32,
+        'super' : str,
+        'prefectures' : str,
+        'municipality' : str,
+        'shop_name_1' :  str,
+        'shop_id_1' :    str,
+        'store_latitude' : np.double,
+        'store_longitude' : np.double,
+        #'年月日' : 'datetime64[ns]',
+        #'天気': str,
+        #'平均気温(℃)': np.float32,
+        #'最高気温(℃)': np.float32,
+        #'最低気温(℃)': np.float32,
+        #'降水量の合計(mm)': np.float32,
+        #'平均風速(m/s)': np.float32,
+        #'平均湿度(％)': np.float32,
+        #'平均現地気圧(hPa)': np.float32,
+        #'平均雲量(10分比)': np.float32,
+        #'降雪量合計(cm)': np.float32,
+        #'日照時間(時間)': np.float32,
+        #'合計全天日射量(MJ/㎡)': np.float32,
+    }
+    df = df.astype(column_types)
+    return df
