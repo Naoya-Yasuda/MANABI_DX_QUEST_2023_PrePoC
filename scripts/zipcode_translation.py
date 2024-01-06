71c7de77e794130c713c5469a5f6e074378a8a44
Naoya-Yasuda <2301330039zz@cyber-u.ac.jp>
2024-01-06 09:31:11 +0900

    クラスタリング微修正

--- scripts/zipcode_translation.py
+++ scripts/zipcode_translation.py
@@ -9,6 +9,8 @@ from pathlib import Path
 if Path.cwd().name == "notebook":
     os.chdir("..")
 
+url = 'http://geoapi.heartrails.com/api/json?method=searchByPostal&postal='
+
 
 def batch_geocode(zipcode):
     """APIを使用して郵便番号のリストをジオコードする"""
@@ -27,6 +29,21 @@ def batch_geocode(zipcode):
     return data['response']['location'][0]['x'], data['response']['location'][0]['y']
 
 
+def save_checkpoint(index, df):
+    """チェックポイントを保存する"""
+    df.to_csv('data/input/user_info_longitude_latitude.csv', index=True)
+    with open('checkpoint.txt', 'w') as f:
+        f.write(str(index))
+
+
+def load_checkpoint():
+    """チェックポイントを読み込む"""
+    if os.path.exists('checkpoint.txt'):
+        with open('checkpoint.txt', 'r') as f:
+            return int(f.read())
+    return 0
+
+
 if __name__ == '__main__':
     # CSVファイルからDataFrameを読み込む
     df = pd.read_csv('data/input/user_info_cleansing.csv',
@@ -35,10 +52,8 @@ if __name__ == '__main__':
     # 欠損値を「N」に置換
     df['zipcode'] = df['zipcode'].fillna('N')
 
-    url = 'http://geoapi.heartrails.com/api/json?method=searchByPostal&postal='
-
-    # APIリクエストをバッチで実行
-    for i in tqdm(range(0, len(df))):
+    start_index = load_checkpoint()  # チェックポイントから開始インデックスを取得
+    for i in tqdm(range(start_index, len(df))):
         # 'N'を含まない郵便番号を作成
         zipcode = [zipcode for zipcode in df['zipcode']
                    [i:i + 1] if zipcode != 'N']
@@ -53,6 +68,9 @@ if __name__ == '__main__':
             df.loc[df['zipcode'] == zipcode, '緯度'] = float(latitude)
 
         if i % 5 == 0:
+            save_checkpoint(i, df)  # 進捗状況を保存
             time.sleep(1)
-    display(df)
-    df.to_csv('data/input/user_info_longitude_latitude.csv', index=True)
+    save_checkpoint(len(df), df)  # 最後に進捗状況を保存
+# TODO: 重複データの削除
+# TODO: 緯度経度がないデータは手で検索し入力する→https://www.tree-maps.com/zip-code-to-coordinate/
+# TODO: 店舗からの距離を計算する→ユーザー基本情報の登録店舗からGPTで住所と郵便番号をWeb検索してもらってCSV化してもらいAPIで距離を計算する
