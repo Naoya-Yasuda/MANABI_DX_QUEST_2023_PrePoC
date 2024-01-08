82a6fd63de97876245cf17f42979228bab8de9fc
sin6160 <s.saito2048@gmail.com>
2024-01-08 23:27:44 +0900

    RPS全データの充填率算出

--- utils/point_history_utils.py
+++ utils/point_history_utils.py
@@ -123,3 +123,141 @@ def delete_blank_from_filename(directory):
         # ファイル名を変更
         os.rename(old_file, new_file)
         print(f"Renamed '{old_file}' to '{new_file}'")
+
+
+def open_point_history_per_shop(super, shop_name_1): 
+    """
+    店舗ごとに分けたpoint_historyファイルを開く関数
+    args:
+        super: スーパー名
+        shop_name_1: 店舗名
+    return:
+        df: pandas.DataFrame
+    """
+
+    df = pd.read_csv(f'data/input/shop_data/point_history_{super}_{shop_name_1}.csv', encoding="utf-8")
+    df = set_dtype(df)
+    df = replace_nan(df)
+    return df
+
+def aggregate_shop_date(df):
+    """
+    point_historyをshop_id、日付ごとにグループ化し、合計値と代表値を計算する関数
+    args:
+        df: データフレーム
+    return:
+        aggregated_df: データフレーム
+    """
+
+    df = set_dtype(df)
+    df = replace_nan(df)
+
+    # shop_idと年月日ごとにグループ化し、合計値と代表値を計算
+    aggregated_df = df.groupby(['shop_id', '年月日']).agg({
+        'amount': 'sum',
+        'amount_kg': 'sum',
+        'point': 'sum',
+        'total_point': 'sum',
+        'total_amount': 'sum',
+        'coin': 'sum',
+        'series_id': 'first',
+        'shop_name': 'first',
+        'リサイクル分類ID': 'first',
+        '支店ID': 'first',
+        'super': 'first',
+        'prefectures': 'first',
+        'municipality': 'first',
+        'shop_name_1': 'first',
+        'shop_id_1': 'first',
+        'store_opening_time': 'first',
+        'store_closing_time': 'first',
+        'rps_opening_time': 'first',
+        'rps_closing_time': 'first',
+        'store_latitude': 'first',
+        'store_longitude': 'first',
+        '年月日': 'first',
+        '天気': 'first',
+        '平均気温(℃)': 'first',
+        '最高気温(℃)': 'first',
+        '最低気温(℃)': 'first',
+        '降水量の合計(mm)': 'first',
+        '平均風速(m/s)': 'first',
+        '平均湿度(％)': 'first',
+        '平均現地気圧(hPa)': 'first',
+        '平均雲量(10分比)': 'first',
+        '降雪量合計(cm)': 'first',
+        '日照時間(時間)': 'first',
+        '合計全天日射量(MJ/㎡)': 'first',
+        'interval_compared_to_previous': 'first',
+        'interval_compared_to_next': 'first',
+    }).reset_index()
+
+    # shop_idと年月日でソート
+    aggregated_df = aggregated_df.sort_values(by=['shop_id', '年月日'])
+
+    return aggregated_df
+
+def aggregate_date(df):
+    """
+    point_historyを日付ごとにグループ化し、合計値と代表値を計算する関数
+    args:
+        df: データフレーム
+    return:
+        aggregated_df: データフレーム
+    """
+
+    df = set_dtype(df)
+    df = replace_nan(df)
+
+    # 集計する列を定義
+    aggregation = {
+        'amount': 'sum',
+        'amount_kg': 'sum',
+        'point': 'sum',
+        'total_point': 'sum',
+        'total_amount': 'sum',
+        'coin': 'sum',
+        'shop_id' : 'first',
+        'shop_name': 'first',
+        'リサイクル分類ID': 'first',
+        '支店ID': 'first',
+        'super': 'first',
+        'prefectures': 'first',
+        'municipality': 'first',
+        'shop_name_1': 'first',
+        'shop_id_1': 'first',
+        'store_opening_time': 'first',
+        'store_closing_time': 'first',
+        'rps_opening_time': 'first',
+        'rps_closing_time': 'first',
+        'store_latitude': 'first',
+        'store_longitude': 'first',
+        '天気': 'first',
+        '平均気温(℃)': 'first',
+        '最高気温(℃)': 'first',
+        '最低気温(℃)': 'first',
+        '降水量の合計(mm)': 'first',
+        '平均風速(m/s)': 'first',
+        '平均湿度(％)': 'first',
+        '平均現地気圧(hPa)': 'first',
+        '平均雲量(10分比)': 'first',
+        '降雪量合計(cm)': 'first',
+        '日照時間(時間)': 'first',
+        '合計全天日射量(MJ/㎡)': 'first',
+        'interval_compared_to_previous': 'max',
+        'interval_compared_to_next': 'max',
+    }
+
+    # 'filling_rate' 列が存在する場合のみ追加
+    if 'filling_rate' in df.columns:
+        aggregation['filling_rate'] = 'max'
+
+    # 'total_amount_kg_per_day' 列が存在する場合のみ追加
+    if 'total_amount_kg_per_day' in df.columns:
+        aggregation['total_amount_kg_per_day'] = 'max'
+
+    # shop_idと年月日ごとにグループ化し、合計値と代表値を計算
+    aggregated_df = df.groupby(['年月日']).agg(aggregation).reset_index()
+    aggregated_df = aggregated_df.sort_values(by=['年月日'])
+    
+    return aggregated_df
